d2ff7e8f83cd5535ac16305e4a69b4307529566a
Azhar <muhammadazharali.17@gmail.com>
2024-11-05 12:55:07 +0500

    Updated image upload logic and progress handling

--- converter/forms.py
+++ converter/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import ImageUpload
+from django.core.exceptions import ValidationError
 
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageUpload
         fields = ['image']
+
+def clean_image(self):
+        image = self.cleaned_data.get('image')
+        if image:
+            if not image.name.lower().endswith(('.jpg', '.jpeg')):
+                raise ValidationError('Please upload a file in JPG or JPEG format.')
+        return image    
--- converter/views.py
+++ converter/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render, redirect
 from .forms import ImageUploadForm
 from .models import ImageUpload
 from .tasks import convert_to_png
+from django.contrib import messages
 
 def upload_image(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            image = form.save()
-            convert_to_png.delay(image.id)
-            return redirect('progress', image_id=image.id)
+            
+            image = request.FILES['image']
+            if image.content_type in ['image/jpeg', 'image/jpg']:
+                image_instance = form.save()  # Save the form and get the instance
+                convert_to_png.delay(image_instance.id)
+                return redirect('progress', image_id=image_instance.id)  # Use the instance's ID
+            else:
+                messages.error(request, 'Please upload a JPG or JPEG image.')
+                return render(request, 'converter/upload.html', {'form': form})
+        else:
+            messages.error(request, 'Form is not valid.')
     else:
         form = ImageUploadForm()
     return render(request, 'converter/upload.html', {'form': form})
@@ -17,3 +26,16 @@ def upload_image(request):
 def progress(request, image_id):
     image = ImageUpload.objects.get(id=image_id)
     return render(request, 'converter/progress.html', {'image': image})
+
+
+
+# def upload_image(request):
+#     if request.method == 'POST':
+#         form = ImageUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             image = form.save()
+#             convert_to_png.delay(image.id)
+#             return redirect('progress', image_id=image.id)
+#     else:
+#         form = ImageUploadForm()
+#     return render(request, 'converter/upload.html', {'form': form})
